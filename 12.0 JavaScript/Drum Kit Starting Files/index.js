4f0f89d6e32bf97dffdfb015606d0eeb1c5812a8
victornext <victorbrs517@gmail.com>
2025-12-16 10:25:55 -0300

    sound

--- 12.0 JavaScript/Drum Kit Starting Files/index.js
+++ 12.0 JavaScript/Drum Kit Starting Files/index.js
@@ -1,11 +1,13 @@
 
-var numberOfDrumButtons =  document.querySelectorAll(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 
 for (var i=0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", handleClick);
-
     function handleClick() {
-        alert("i got click");
-    } 
+        var audio = new Audio('./sounds/snare.mp3');
+      
+        audio.play(); 
+}
+
 }
