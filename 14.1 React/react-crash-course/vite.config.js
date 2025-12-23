0fdc0c51c2d36315e97ff67eb01acaa73eb0c080
victornext <victorbrs517@gmail.com>
2025-12-23 10:46:59 -0300

    Tailwind instalado

--- 14.1 React/react-crash-course/vite.config.js
+++ 14.1 React/react-crash-course/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { 
+    port: 3000, //Por padrao a porta vai ser 5173, alterando agora pra 3000
+  },
 })
